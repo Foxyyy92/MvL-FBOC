e115b719d5801079a8439efe60b062ce8af0b0b1
Foxyyy92 <Foxyyy92@users.noreply.github.com>
2025-08-07 19:03:00 -0700

    my ASS!! (0.6 Reborn)
    ghostly manor finished, castle music. yadda yadda go die.

--- Assets/Scripts/BuildInfo.cs
+++ Assets/Scripts/BuildInfo.cs
@@ -1,1 +1,1 @@
-public static class BuildInfo{public static string BUILD_TIME = "02/08/2025 12:40:11 a. m.";}
+public static class BuildInfo{public static string BUILD_TIME = "08/08/2025 01:38:46 a. m.";}
