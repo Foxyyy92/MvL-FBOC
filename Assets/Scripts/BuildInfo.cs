5d1d967d64a4dad6613a736e27410079dede54cb
Foxyyy92 <Foxyyy92@users.noreply.github.com>
2025-09-01 15:11:34 -0700

    Castle Rooftop or something.. i dont care anymore man

--- Assets/Scripts/BuildInfo.cs
+++ Assets/Scripts/BuildInfo.cs
@@ -1,1 +1,1 @@
-public static class BuildInfo{public static string BUILD_TIME = "08/08/2025 01:38:46 a. m.";}
+public static class BuildInfo{public static string BUILD_TIME = "01/09/2025 04:09:46 a. m.";}
